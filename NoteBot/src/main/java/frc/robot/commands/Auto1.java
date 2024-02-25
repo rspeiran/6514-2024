6030fd9011b7943368b2335df468c17ef13689d5
Rob Speiran <rspeiran@outlook.com>
2024-02-25 13:44:43 -0500

    Auto1 Testing

--- NoteBot/src/main/java/frc/robot/commands/Auto1.java
+++ NoteBot/src/main/java/frc/robot/commands/Auto1.java
@@ -2,21 +2,19 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.ConveyorSubsystem;
 import frc.robot.subsystems.DriveSubsystem;
 
-
 public class Auto1 extends SequentialCommandGroup {
 
-    //private final ConveyorSubsystem m_conveyorSubsystem;
-    //private final DriveSubsystem m_driveSubsystem;
+    private final ConveyorSubsystem m_conveyorSubsystem;
+    private final DriveSubsystem m_driveSubsystem;
 
-    public Auto1() {
-
-        //ConveyorSubsystem conveyorSubsystem, DriveSubsystem driveSubsystem
-       // m_conveyorSubsystem = conveyorSubsystem;
-       // m_driveSubsystem = driveSubsystem;
-
+    public Auto1(ConveyorSubsystem cs, DriveSubsystem ds) {
+    //public Auto1()  {
+        m_conveyorSubsystem = cs;
+        m_driveSubsystem = ds;
 
     addCommands(
         // Add Commands here:
@@ -30,13 +28,17 @@ public class Auto1 extends SequentialCommandGroup {
         //      )    
         //  );
         //  STEP 1:   Shoot
-        //new ShootSpeakerHigh(m_conveyorSubsystem, 7.0),
+        new WaitCommand(2),
+        new ShootSpeakerHigh(m_conveyorSubsystem, 7.0),
+        new WaitCommand(2),
         //  STEP 2:     Forward 6 * 12 = 72 (Pickup)
-        //new DriveForwardMeters(72, 0.40, m_driveSubsystem),
+        new DriveForwardMeters(72, 0.60, m_driveSubsystem),   //MOVED 95 INCHES
+        new WaitCommand((0.25)),
         //  STEP 3:     Backwars 6 * 12 = 72
-        //new DriveForwardMeters(-72, 0.40, m_driveSubsystem),
+        new DriveForwardMeters(72, -0.60, m_driveSubsystem),
+        new WaitCommand(0.25),
         //  STEP 4:  Shoot
-        //new ShootSpeakerHigh(m_conveyorSubsystem, 7.0)
+        new ShootSpeakerHigh(m_conveyorSubsystem, 7.0)
 
         );
     }
